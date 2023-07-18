7b8eaafc79b3be6182a68d71cdfcef5a8c5a8b02
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-18 17:55:22 -0300

    chore: returning handlesubmit function

--- src/pages/payment/components/ButtonCancel.tsx
+++ src/pages/payment/components/ButtonCancel.tsx
@@ -2,11 +2,15 @@ import { useNavigate } from "react-router-dom";
 export function ButtonCancel() {
   const navigate = useNavigate();
 
+  const handleSubmit = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <button
         type="button"
-        onClick={() => navigate("/")}
+        onClick={handleSubmit}
         className=" mt-2 flex w-60 h-12 items-center text-center justify-center cursor-pointer bg-red-400 hover:bg-opacity-90 text-white rounded-lg"
       >
         Cancel
