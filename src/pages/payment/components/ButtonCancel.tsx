4ef00c3f8ed0d161e5991e4e3f2fc3365b63148c
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-18 13:51:04 -0300

    chore: refactoring

--- src/pages/payment/components/ButtonCancel.tsx
+++ src/pages/payment/components/ButtonCancel.tsx
@@ -2,15 +2,11 @@ import { useNavigate } from "react-router-dom";
 export function ButtonCancel() {
   const navigate = useNavigate();
 
-  const handleOnClick = () => {
-    navigate("/");
-  };
-
   return (
     <>
       <button
         type="button"
-        onClick={handleOnClick}
+        onClick={() => navigate("/")}
         className=" mt-2 flex w-60 h-12 items-center text-center justify-center cursor-pointer bg-red-400 hover:bg-opacity-90 text-white rounded-lg"
       >
         Cancel
