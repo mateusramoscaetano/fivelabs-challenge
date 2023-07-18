e283df9f22b7f8ca17ac91aae3a50e220a0fb5e1
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-18 13:50:51 -0300

    feat: errors adjustment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 import api from "../../data/services/api";
 
 import { Vehicle } from "../../@types";
-import { CardStarship, Header } from "../../components";
+import { CardStarship } from "../../components";
+import { toastError } from "../../helpers";
 
 export function Home() {
-  const [starships, setStarships] = useState<Vehicle[]>([]);
+  const [starships, setStarships] = useState<Vehicle[]>([] as Vehicle[]);
 
   async function getAllVehicles() {
     try {
       const response = await api.get("/starships");
+      const { data } = response;
 
-      setStarships(response.data.results);
+      setStarships(data.results);
     } catch (error) {
-      console.error(error);
+      toastError("Internal Server Error");
     }
   }
 
@@ -23,9 +25,8 @@ export function Home() {
 
   return (
     <>
-      <div className="bg-[url('/sky.jpg')] bg-repeat-y bg-cover  bg-center flex-1  w-screen min-h-screen">
-        <Header />
-        <main className="mt-12 pl-48 pr-48 w-screen min-h-screen grid grid-cols-3 justify-center">
+      <div className="  bg-[url('/sky.jpg')]  bg-cover bg-repeat  flex-1  w-full min-h-screen">
+        <main className="pl-48 pr-48 w-screen min-h-screen grid grid-cols-3 justify-center">
           {starships.map((starship) => (
             <CardStarship
               key={starship.name}
