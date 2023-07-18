73f2d8bce7739283b9557bd7d493c730cd3dda80
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-18 13:48:04 -0300

    feat: header added to routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,13 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/home";
 import { CheckoutPage } from "../pages/checkout";
 import { FormPayment } from "../pages/payment";
+import { Header } from "../components";
 
 export default function MainRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route
+          path="/"
+          element={
+            <Header>
+              <Home />
+            </Header>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            <Header>
+              <CheckoutPage />
+            </Header>
+          }
+        />
         <Route path="/payment" element={<FormPayment />} />
       </Routes>
     </Router>
